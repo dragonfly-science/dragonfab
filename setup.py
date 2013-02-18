07d3b775eda4d84e0a8e1105baea2f9046c57249
Finlay Thompson <finlay@dragonfly.co.nz>
2013-02-18 14:23:13 +1300

    Added basic setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name = "dragonfab",
+    version = "1.0",
+    description = "Fabric support",
+    author = "Joel Pitt",
+    author_email = "joel@joelpitt.com",
+    url = "https://github.com/ferrouswheel/dragonfab",
+    packages = ['dragonfab'],
+) 
